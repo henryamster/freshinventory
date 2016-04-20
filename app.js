f6463d14d19dfb5e6514c371fcdfaab22fd5cc6e
henry fritz <henryamsterfritz@gmail.com>
2016-04-20 19:55:13 +0000

    Save before introducing new controllers and refactoring product controller

--- app.js
+++ app.js
@@ -1,44 +1,70 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var MongoClient = require('mongodb').MongoClient;
+var mongoose = require('mongoose'); 
 console.log('server is up!');
 
-
+app.use(express.static('client'));
 app.use(bodyParser.urlencoded({extended: true}));
-var db;
-MongoClient.connect('mongodb://localhost:27017/test2', (err, database) => {
-    if (err) throw err;
-    db = database;
-    app.listen(process.env.PORT, process.env.IP);
+app.use(bodyParser.json());                                     
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
+//connect to mongodb
+mongoose.connect('mongodb://localhost:27017/test2')
+  //DELETE  //app.listen(process.env.PORT, process.env.IP);
     
-})
+    // define model =================
+    var Product = mongoose.model('Product', {
+        name : String,
+        upc : Number,
+        category : String
+    });
 
-app.post('/', (req, res) => {
-  db.collection('plist').save(req.body, (err, result) => {
-    if (err) return console.log(err)
 
-    console.log('Item Added!')
-    res.redirect('/')
-  })
-})
-
-app.get('/', function (req, res) {
-      res.sendfile(__dirname + '/client/index.html');
-      var cursor = db.collection('quotes').find();
-     db.collection('plist').find().toArray(function(err, results) {
-  
+//view route
+app.get('/products', function (req, res) {
+      Product.find(function(err, items){
+          if (err) throw err;
+          res.json(items);
+      });
 });
+//post route
+app.post('/products', function (req, res){
+    Product.create({
+        name : req.body.name,
+        upc : req.body.upc,
+        category : req.body.category
+    }), function(err, product) {
+        if (err) throw err;
+        Product.find(function(err, products){
+            if (err) throw err;
+            res.json(products);
+        })
+    }
+});
+//delete route
+app.delete('/products/:product_id', function (req, res){
+    Product.remove({
+        _id : req.params.product_id }, function(err, product){
+            if (err) throw err;
+            Product.find(function(err, products){
+                if (err) throw err;
+                res.json(products);
+            })
+        }
+    )
+});
+      //route to angular app
+app.get('/', function (req, res){
+    res.sendfile(__dirname + '/client/index.html');
+});
+      
+      //res.sendfile(__dirname + '/client/index.html');
+      //var cursor = db.collection('quotes').find();
+     //db.collection('plist').find().toArray(function(err, results) {
+  
 
-})
-
-
-//app.post('/', function (req, res) {
-    //res.send(req.body);
-    //console.log('lol' + req.body);
-    
-//});
 
 
 
-//app.listen(process.env.PORT, process.env.IP);
+
+app.listen(process.env.PORT, process.env.IP);
---
+++ client/js/core.js
@@ -0,0 +1,23 @@
+var productList = angular.module('productList', []);
+
+function productController($scope, $http){
+    $scope.formData = {};
+    $http.get('/products').success(function(data){
+        $scope.products = data;
+        console.log(data);
+    }).error(function(data){
+        console.log(data);
+    });
+    $scope.createProduct = function() {
+        $http.post('/products', $scope.formData).success(function(data){
+            $scope.products = data;
+            $scope.formData = {};
+          }).error(function(data){console.log(data);})
+    };
+    $scope.deleteProduct = function(id){
+        $http.delete('/products/' + id).success(function(data){
+            $scope.products = data;
+        }).error(function(data){console.log(data)});
+    }
+    
+}
